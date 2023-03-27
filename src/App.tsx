abc50b3fd2157f97ff5eb54b02623a0eb9e24cbd
arleysouza <arley.ferreira.souza@gmail.com>
2023-03-27 18:36:55 -0300

    código

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import A from "./components/A";
+import Contexto from "./contexts/Contexto";
 
 function App() {
+  const [nome,setNome] = useState("");
+  const [idade,setIdade] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>Nome</label>
+        <input value={nome} onChange={(e)=>setNome(e.target.value)} />
+      </div>
+      <div>
+        <label>Idade</label>
+        <input value={idade} onChange={(e)=>setIdade(e.target.value)} />
+      </div>
+      <Contexto.Provider value={{nome,idade}}>
+        <A />
+      </Contexto.Provider>
     </div>
   );
 }
